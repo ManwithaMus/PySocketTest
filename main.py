5ba0f0d28771495d8c4666a852fb8a7a7ded15a0
Aron S <siposbalint@yahoo.com>
2020-04-22 19:31:45 +0200

    First working Socket

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ Created on Apr 22, 2020
 @author: aron
 '''
 import socket
-
-   
+ 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((socket.gethostname(), 1234))
-sock.listen(3)
+sock.listen(5)
 
 while True:
-    clientsocket = sock.accept()
-    address = sock.accept()
+    clientsocket, address = sock.accept()
     print(f"The Ip connecting to is {address}!")
     clientsocket.send(bytes("Hello Gordan!", "utf-8"))
+    clientsocket.close()
