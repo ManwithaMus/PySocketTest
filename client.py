b4cacbe673f69ef8e0acd3cb8586530d2329845a
Aron S <siposbalint@yahoo.com>
2020-04-22 19:46:26 +0200

    Revert "First working Socket"
    This reverts commit 5ba0f0d28771495d8c4666a852fb8a7a7ded15a0.

--- client.py
+++ client.py
@@ -7,5 +7,6 @@ import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((socket.gethostname(), 1234))
+
 msg = sock.recv(1024)
 print(msg.decode("utf-8"))
--- main.py
+++ main.py
@@ -4,13 +4,14 @@ Created on Apr 22, 2020
 @author: aron
 '''
 import socket
- 
+
+   
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((socket.gethostname(), 1234))
-sock.listen(5)
+sock.listen(3)
 
 while True:
-    clientsocket, address = sock.accept()
+    clientsocket = sock.accept()
+    address = sock.accept()
     print(f"The Ip connecting to is {address}!")
     clientsocket.send(bytes("Hello Gordan!", "utf-8"))
-    clientsocket.close()
