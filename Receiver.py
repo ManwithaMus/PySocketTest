02f29ff90963b09b3ac0163462fdae9b4249b902
Aron S <siposbalint@yahoo.com>
2020-04-22 23:27:05 +0200

    First working version with Names changed

--- Receiver.py
+++ Receiver.py
@@ -4,9 +4,8 @@ Created on Apr 22, 2020
 @author: aron
 '''
 import socket
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((socket.gethostname(), 1234))
-
 msg = sock.recv(1024)
 print(msg.decode("utf-8"))
+sock.close()
--- Sender.py
+++ Sender.py
@@ -4,14 +4,14 @@ Created on Apr 22, 2020
 @author: aron
 '''
 import socket
+text = input("What do you want to send over?\n")
 
-   
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((socket.gethostname(), 1234))
-sock.listen(3)
+sock.listen(1)
 
-while True:
-    clientsocket = sock.accept()
-    address = sock.accept()
-    print(f"The Ip connecting to is {address}!")
-    clientsocket.send(bytes("Hello Gordan!", "utf-8"))
+print("Waiting for connection!")
+clientsocket, address = sock.accept()
+print("Connection has been established")
+clientsocket.send(bytes(text, "utf-8"))
+sock.close()
